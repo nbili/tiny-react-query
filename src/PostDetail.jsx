d09b21ee3e882970e031c21c6bc1cad4247ec061
nbili <coding520@163.com>
2022-04-30 23:10:47 +0800

    use

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -22,7 +25,14 @@ async function updatePost(postId) {
 }
 
 export function PostDetail({ post }) {
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery(
+    ["comments", post.id],
+    () => fetchComments(post.id)
+  );
+
+  if (isLoading) return <h1>Loading ...</h1>;
+
+  if (isError) return <>Oops {error.toString()}</>;
 
   return (
     <>
--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,25 +1,44 @@
 import { useEffect, useState } from "react";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 
 import { PostDetail } from "./PostDetail";
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0`
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
 
   return response.json();
 }
 
+const maxPage = 10;
+
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data, isError, isLoading, error } = useQuery("posts", fetchPosts, {
-    staleTime: 3000,
-  });
+  const queryClient = useQueryClient();
 
-  if (isLoading) return <h1>Loading</h1>;
+  useEffect(() => {
+    if (currentPage < maxPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery(["posts", nextPage], () =>
+        fetchPosts(nextPage)
+      );
+    }
+  }, [currentPage, queryClient]);
+
+  const { data, isError, isLoading, isFetching, error } = useQuery(
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    {
+      staleTime: 2000,
+      refetchOnWindowFocus: false,
+      keepPreviousData: true,
+    }
+  );
+
+  if (isFetching) return <h1>Fetching ...</h1>;
 
   if (isError) return <h2>Oops {error.toString()}</h2>;
 
@@ -37,9 +56,23 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button onClick={() => {}}>Previous page</button>
-        <span>Page {currentPage + 1}</span>
-        <button onClick={() => {}}>Next page</button>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((pre) => pre - 1);
+          }}
+        >
+          Previous page
+        </button>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPage}
+          onClick={() => {
+            setCurrentPage((pre) => pre + 1);
+          }}
+        >
+          Next page
+        </button>
       </div>
       <hr />
 
